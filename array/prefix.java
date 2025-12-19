c31a0bba035a6a95f5fd418cd7e2e7dfeeb19601
sunny chahal <sunnychaha555@gmail.com>
2025-12-19 19:15:57 +0530

    Trapping Rain Water

--- array/prefix.java
+++ array/prefix.java
@@ -1,16 +1,16 @@
 public class prefix{
-    public static int maxSum(int[] array)
+    public static int maxSum(int[] a)
     {
         int max = 0;
-        int prefix[] = new int[array.length];
-        prefix[0] = array[0];
-        for (int i = 1; i < array.length; i++)
+        int prefix[] = new int[a.length];
+        prefix[0] = a[0];
+        for (int i = 1; i < a.length; i++)
             {
-                prefix[i] = prefix[i - 1] + array[i];
+                prefix[i] = prefix[i - 1] + a[i];
             }
-        for (int i = 0; i < array.length; i++)
+        for (int i = 0; i < a.length; i++)
             {
-                for (int j = i; j < array.length; j++)
+                for (int j = i; j < a.length; j++)
                     {
                         int sum = i == 0 ? prefix[j]: prefix[j] - prefix[i - 1];
                         if (max < sum)
---
+++ array/trapwater.java
@@ -0,0 +1,31 @@
+public class trapwater {
+    public static int trapWater (int[] arr)
+    {
+        int[] Left_max = new int[arr.length];
+        Left_max[0] = arr[0];
+        for(int i = 1; i < arr.length; i++)
+            {
+                Left_max[i] = Math.max(Left_max[i - 1], arr[i]);
+            }
+        int[] Right_max = new int[arr.length];
+        Right_max[arr.length - 1] = arr[arr.length - 1];
+        for(int i = (arr.length - 2); i >= 0; i--)
+            {
+                Right_max[i] = Math.max(Right_max[i + 1], arr[i]);
+            }
+        int Water_trap = 0;
+        for (int i = 0; i < arr.length; i++)
+            {
+                    int s = Math.min(Right_max[i], Left_max[i]);
+                            int trap = s - arr[i];
+                            Water_trap += trap;
+                         }
+            return Water_trap;
+    }
+    public static void main(String args[])
+    {
+        int[] arr = {4, 2, 0, 6, 3, 2, 5};
+        int Water = trapWater(arr);
+        System.out.println("Trapped Water: " + Water);
+    }
+}
