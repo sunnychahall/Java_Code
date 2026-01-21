f2e23de4bdb56865fd1bcb9267e44cecad00c54d
sunny chahal <sunnychaha555@gmail.com>
2026-01-21 19:49:29 +0530

    O(m + n) Staircase Search

---
+++ 2Darray/spiral.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class spiral {
+    public static void printSpiral(int matrix[][])
+    {
+        int startCol = 0;
+        int endCol = matrix[0].length - 1;
+        int startRow = 0;
+        int endRow = matrix.length - 1;
+        while (startCol <=  endCol && startRow <= endRow)
+    {
+        // top (left to ringht)
+        for (int i = startCol; i <= endCol; i++)
+            {
+                System.out.print(matrix[startRow][i] + " ");
+            }
+        // right (top to down)
+        for (int i = startRow + 1; i <= endRow; i++)
+            {
+                System.out.print(matrix[i][endCol] +" ");
+            }
+        // down (left to right)
+        for (int i = endCol - 1; i >= startCol; i--)
+            {
+                if (endRow == startRow)
+                    {
+                        break;
+                    }
+                System.out.print(matrix[endRow][i] +" ");
+            }
+        for (int i = endRow - 1; i > startRow; i--)
+            {
+                if (endCol == startCol)
+                    {
+                        break;
+                    }
+                    System.out.print(matrix[i][startCol] +" ");
+            }
+            endCol--;
+            endRow--;
+            startCol++;
+            startRow++;
+    }
+    System.out.println();
+}
+    public static void main(String args[])
+    {
+        System.out.print("Enter no of Rows: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.print("Enter no of Columns: ");
+        int m = scanner.nextInt();
+        int Matrix[][] = new int[n][m];
+        System.out.println("Enter elements of matrix: ");
+        for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                    {
+                        Matrix[i][j] = scanner.nextInt();
+                    }
+            }
+        printSpiral(Matrix);
+    }
+}
