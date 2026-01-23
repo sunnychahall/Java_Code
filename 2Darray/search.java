051176169eba2026d652fad1ba516f7e7ec6cc8e
sunny chahal <sunnychaha555@gmail.com>
2026-01-23 06:44:40 +0530

    O(m + n)

--- 2Darray/search.java
+++ 2Darray/search.java
@@ -1,3 +1,37 @@
+import java.util.*;
+
 public class search {
-    
-}
+    public static boolean Search(int matrix[][], int key)
+    {
+        int row = matrix.length - 1;
+        int col = 0;
+        while (row >= 0 && col <= matrix[0].length - 1)
+            {
+                if (key == matrix[row][col])
+                    {
+                        System.out.println("Key found at index : " + row +", " + col);
+                        return true;
+                    }
+                else if (key < matrix[row][col])
+                    {
+                        row--;
+                    }
+                else 
+                    {
+                        col++;
+                    }
+                }
+                System.out.println("Key not found");
+                return false;
+            }
+            public static void main(String args[])
+            {
+                int matrix[][] = {{1, 2, 3},
+                                   {4, 8, 9}};
+                System.out.print("Enter Value: ");
+                Scanner scanner = new Scanner(System.in);
+                int value = scanner.nextInt();
+                Search(matrix, value);
+            }
+    }
+
