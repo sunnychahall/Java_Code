70d6d8cafe5455be57960d0b2fcaaa2b8c70e237
sunny chahal <sunnychaha555@gmail.com>
2026-02-03 20:20:31 +0530

    Bits

--- Bit/bit.java
+++ Bit/bit.java
@@ -27,11 +27,26 @@ public class bit {
 
     }
 
+    public static int clearLastBit(int n, int i)
+    {
+        int Bitmask = (~0) << i;
+        return Bitmask & n;
+    }
+
+    public static int clearRange(int n, int i, int j)
+    {
+        int a = ((~0) << j + 1);
+        int b = ((1 << i) - 1);
+        return (a | b) & n;
+    }
+
     public static void main(String args[])
     {
         System.out.println(Get_ith_Bit(4 , 2));
         System.out.println(set_Bit(4, 2));
         System.out.println(clearBit(4, 2));
         System.out.println(UpdateBit(4, 2, 0));
+        System.out.println(clearLastBit(4, 2));
+        System.out.println(clearRange(5, 0, 1));
     }
 }
