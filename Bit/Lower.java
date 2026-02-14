2d5bc6be209d07befc137f2e0a17a3ed9a3e898d
sunny chahal <sunnychaha555@gmail.com>
2026-02-14 22:13:46 +0530

    ' ' == 32 hence we use both char ' ' or int 32

---
+++ Bit/Lower.java
@@ -0,0 +1,10 @@
+public class Lower {
+    public static void main(String args[])
+    {
+        for (char ch = 'A'; ch <= 'Z'; ch++)
+            {
+                System.out.print((char)(ch | ' '));
+            }
+            System.out.println();
+    }
+}
