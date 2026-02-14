23c11a691c9ff8ba896fc12296766d2e94fc2b4e
sunny chahal <sunnychaha555@gmail.com>
2026-02-14 21:58:38 +0530

    Using 2's Complemrnt relation

---
+++ Bit/AddOne.java
@@ -0,0 +1,11 @@
+public class AddOne {
+    public static void main(String args[])
+    {
+
+        // -x = ~x + 1
+        // hence -(~x) = x + 1
+        int x = 4;
+        System.out.println(-(~x));
+
+    }
+}
