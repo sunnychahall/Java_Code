19e21486d92de9c0eae9d92a4f1dfa1453e65cd8
sunny chahal <sunnychaha555@gmail.com>
2026-02-21 11:54:37 +0530

    O(n)

--- Recursion/numbers.java
+++ Recursion/numbers.java
@@ -3,15 +3,19 @@ public class numbers
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Number (Positive): ");
         int n = scanner.nextInt();
+        
         System.out.println("Numbers in Increasing Order: ");
         print_n(n);
+        System.out.println("Factorial of n: " + n);
+        System.out.println(factorial(n));
     }
 
     public static void print_n(int n)
     {
         
-        if (n == 1)
+        if (n == 1 || n == 0)
             {
                 System.out.println(n);
                 return;
@@ -20,6 +24,16 @@ public class numbers
             // print before recursion call for print in decreasing order
         print_n(n - 1);
         System.out.println(n);
+        
+    }
+
+    public static int factorial(int n)
+    {
+        if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+        return n * factorial(n - 1);
         
     }
 }
