f2259e3a3e612e6a7f0f8fd503fb39c3fd5845e0
sunny chahal <sunnychaha555@gmail.com>
2026-02-24 21:28:45 +0530

    O(n)

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,9 +1,13 @@
-public class isSorted{
+public class Recursion{
     public static void main(String[] args)
     {
         int[] arr = {2, 4, 5, 7, 1};
         System.out.println(isSort(arr, 0));
+        System.out.println(power(5, 3));
+
     }
+
+    
 
     public static boolean isSort(int[] arr, int n)
     {
@@ -16,5 +20,14 @@ public class isSorted{
                 return false;
             }
         return isSort(arr, n + 1);
+    }
+
+    public static int power(int x, int n)
+    {
+        if (n == 0)
+            {
+                return 1;
+            }
+        return x * power(x, n - 1); 
     }
 }
