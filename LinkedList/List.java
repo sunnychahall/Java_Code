90f82947565325a27313baee077c9ca89f94b013
sunny chahal <sunnychaha555@gmail.com>
2026-03-18 17:56:22 +0530

    LinkedBasics

---
+++ LinkedList/List.java
@@ -0,0 +1,103 @@
+public class List {
+
+    public class Node{
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data =  data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data)
+    {
+       Node new_Node = new Node(data);
+       if ( head == null)
+        {
+            head = tail = new_Node;
+            size++;
+            return;
+
+        }
+
+        new_Node.next = head;
+        head = new_Node;
+        size++;
+    }
+
+    public void addLast(int data)
+    {
+        Node new_Node = new Node(data);
+        if (head == null)
+            {
+                head = tail = new_Node;
+                size++;
+                return;
+            }
+            tail.next = new_Node;
+            tail = new_Node;
+            size++;
+    }
+
+    public void AddINDEX(int data, int index)
+    {
+        if (index < 0 || index > size)
+            {
+                System.out.println("Not exist");
+                return;
+            }
+        int i = 0;
+         
+        Node temp = head;
+        Node new_Node = new Node(data);
+        while(i < index - 1)
+            {
+                temp = temp.next;
+                i++;
+            }
+
+            new_Node.next = temp.next;
+            temp.next = new_Node;
+            size++;         
+
+     }
+
+    public void print()
+    {
+        Node temp = head;
+        while(temp != null)
+            {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+
+    
+    public static void main(String[] args) {
+        size = 0;
+       List ll = new List();
+
+        ll.addFirst(53);
+        ll.addFirst(43);
+        ll.addFirst(23);
+        ll.addFirst(73);
+        ll.addFirst(53);
+        ll.addLast(57);
+        ll.addLast(56);
+        ll.addLast(55);
+        ll.addLast(5);
+        ll.AddINDEX(3, 3);
+        ll.print();
+        System.out.println(size);
+        
+    }
+}
+
+
